8046e2e3918addf9733a830cb1409ad3019c63f9
Eshaan <61537298+eshdownbad@users.noreply.github.com>
2023-12-17 17:52:47 +0530

    added basic info stuff

--- app.vue
+++ app.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="container mx-auto">
-    <section class="bg-slate-400 p-10 mt-10 rounded-xl shadow-lg shadow-slate-600 w-1/2 mx-auto">
-      <h1 class="text-4xl font-bold text-center  text-white dark:text-gray-800">Coming Soon!</h1>
+    <section
+      class="mx-auto mt-10 flex w-full flex-row gap-8 rounded-xl bg-slate-400 p-8 shadow-lg shadow-slate-700 md:w-3/4"
+    >
+      <div
+        class="flex flex-1 flex-col justify-center rounded-2xl bg-gray-800 text-center text-white"
+      >
+        <h1 class="text-3xl">Eshaan</h1>
+      </div>
+      <div class="flex-1">
+        <div class="flex flex-col gap-2">
+          <a
+            v-for="linkData in LINKS"
+            :key="linkData.link"
+            class="rounded-2xl border-2 border-gray-800 p-3 text-center text-xl font-bold text-gray-800 transition-colors duration-300 hover:bg-gray-800 hover:text-white"
+            :href="linkData.link"
+          >
+            {{ linkData.title }}
+          </a>
+        </div>
+      </div>
     </section>
   </div>
 </template>
 <script lang="ts" setup>
-
+const LINKS = [
+  {
+    title: 'Twitter',
+    link: 'https://twitter.com/eshdownbad',
+  },
+  {
+    title: 'Github',
+    link: 'https://github.com/eshdownbad',
+  },
+  {
+    title: 'Email',
+    link: 'mail:contact@eshaan.dev',
+  },
+] as const;
 useHead({
   bodyAttrs: {
-    "class": "dark:bg-gray-800"
-  }
-})
+    class: 'dark:bg-gray-900',
+  },
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon' , '@nuxtjs/google-fonts'],
   devtools: { enabled: true },
+  googleFonts: {
+    families: {
+      inter: true,
+    }
+  }
 });
