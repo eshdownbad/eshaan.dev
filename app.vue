e7a15ef4587b8c6b66fd8b15ab8a1f4b16f8a481
Eshaan <61537298+eshdownbad@users.noreply.github.com>
2023-12-17 15:24:17 +0530

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <section class="bg-slate-400 p-10 mt-10 rounded-xl shadow-lg shadow-slate-600 w-1/2 mx-auto">
+      <h1 class="text-4xl font-bold text-center  text-white dark:text-gray-800">Coming Soon!</h1>
+    </section>
+  </div>
+</template>
+<script lang="ts" setup>
+
+useHead({
+  bodyAttrs: {
+    "class": "dark:bg-gray-800"
+  }
+})
+</script>
